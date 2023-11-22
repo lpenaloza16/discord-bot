d02895520df008c8fbef624642fd3826fee11787
Leo J Penaloza <39505025+lpenaloza16@users.noreply.github.com>
2023-11-22 06:20:47 -0800

    added try catch and safety

--- src/events/InteractionCreate.js
+++ src/events/InteractionCreate.js
@@ -1,6 +1,33 @@
 async function interactionCreateHandler(interaction) {
+  if (!interaction.isChatInputCommand()) {
+    return;
+  }
+
   const command = interaction.client.commands.get(interaction.commandName);
-  await command.execute(interaction);
+  if (!command) {
+    return;
+  }
+
+  try {
+    await command.execute(interaction);
+
+    console.log(
+      `${interaction.user.username} used command ${interaction.commandName}`
+    );
+  } catch (errors) {
+    console.error(error);
+    if (interaction.replied || interaction.deffered) {
+      await interaction.followUp({
+        content: "There was an error executing this command",
+        ephemeral: true,
+      });
+    } else {
+      await interaction.reply({
+        content: "There was an error executing this command",
+        ephemeral: true,
+      });
+    }
+  }
 }
 
 module.exports = {
